7625c192601ec4d76da6f57e8147374262d69fea
ikram leila hamzaoui <128831018+ikramleilahm@users.noreply.github.com>
2026-02-06 22:50:20 +0100

    feat: ajout des trois scenarios de test + update version npm

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -54,4 +54,49 @@ describe('Feature : Change seats', () => {
       await expect(result).rejects.toThrow('Webinar not found');
     });
   });
+
+  describe('Scenario: update the webinar of someone else', () => {
+    const payload = {
+      user: testUser.bob,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+
+    it('should fail', async () => {
+      const result = useCase.execute(payload);
+      await expect(result).rejects.toThrow(
+        'User is not allowed to update this webinar',
+      );
+    });
+  });
+
+  describe('Scenario: change seat to an inferior number', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 50,
+    };
+
+    it('should fail', async () => {
+      const result = useCase.execute(payload);
+      await expect(result).rejects.toThrow(
+        'You cannot reduce the number of seats',
+      );
+    });
+  });
+
+  describe('Scenario: change seat to a number > 1000', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 1001,
+    };
+
+    it('should fail', async () => {
+      const result = useCase.execute(payload);
+      await expect(result).rejects.toThrow(
+        'Webinar must have at most 1000 seats',
+      );
+    });
+  });
 });
